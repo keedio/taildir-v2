425da3da6ad32d60083ad86da44c4dba3a5263a5
Luca Rosellini <luca.rosellini@gmail.com>
2015-10-29 14:14:34 +0100

    Added missing metrics

--- src/main/java/org/keedio/flume/source/watchdir/metrics/MetricsMBean.java
+++ src/main/java/org/keedio/flume/source/watchdir/metrics/MetricsMBean.java
@@ -6,9 +6,30 @@ import javax.management.MXBean;
 public interface MetricsMBean {
 
 	
-	public long getTotalEvents();
-	public long getTotalFiles();
-	public double getMeanProcessTime();
-	public double getMeanFileEvents();
-	
+	long getTotalEvents();
+	double getEventsFifteenMinuteRate();
+	double getEventsFiveMinuteRate();
+	double getEventsOneMinuteRate();
+	double getEventsMeanRate();
+
+	long getTotalFiles();
+	double getFilesFifteenMinuteRate();
+	double getFilesFiveMinuteRate();
+	double getFilesOneMinuteRate();
+	double getFilesMeanRate();
+
+	double getMeanProcessTime();
+	double get99thPercentileProcessTime();
+	double get75thPercentileProcessTime();
+	long getMaxProcessTime();
+	long getMinProcessTime();
+	double getMedianProcessTime();
+
+
+	double getMeanFileEvents();
+	double get99thPercentileFileEvents();
+	double get75thPercentileFileEvents();
+	long getMaxFileEvents();
+	long getMinFileEvents();
+	double getMedianFileEvents();
 }
